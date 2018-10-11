7f3d74fa6606ecba84bd23ead8fc7153396ba7c3
Anton Generalov <ageneralov@uchicago.edu>
2018-10-11 16:21:20 -0500

    Adding partner 2 changes

--- java/src/edu/virginia/engine/display/AnimatedSprite.java
+++ java/src/edu/virginia/engine/display/AnimatedSprite.java
@@ -31,5 +31,28 @@ public class AnimatedSprite extends Sprite {
             this.gameClock = new GameClock();
     }
 
+    /* Partner 2 - Part 3 and 4 */
+    public void animate(Animation animateObject) {
+        this.startFrame = animateObject.getStartFrame();
+        this.endFrame = animateObject.getEndFrame();
+    }
+    public void animate(String id) {
+
+
+    }
+    public void animate(Integer startFrame, Integer endFrame) {
+        this.startFrame = startFrame;
+        this.endFrame = endFrame;
+    }
+
+    public void stopAnimation(Integer frameNumber) {
+
+    }
+    public void stopAnimation() {
+
+    }
+
+
+
 
 }
