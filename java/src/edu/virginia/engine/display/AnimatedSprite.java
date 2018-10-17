4aed2c08592805c0d40731b45a43c41150f4ea68
Krishan Rajaratnam <krishanrajaratnam@Krishans-MacBook-Pro.local>
2018-10-17 18:10:47 -0500

    Lab 2 working

--- java/src/edu/virginia/engine/display/AnimatedSprite.java
+++ java/src/edu/virginia/engine/display/AnimatedSprite.java
@@ -5,28 +5,30 @@ import edu.virginia.engine.util.GameClock;
 import java.util.ArrayList;
 import java.awt.*;
 import java.lang.Thread;
+import java.util.NoSuchElementException;
 
 public class AnimatedSprite extends Sprite {
 
     static final int DEFAULT_ANIMATION_SPEED = 1000;
 
     private ArrayList<Animation> animations;
-    private Boolean playing;
+    private Boolean playing = false;
     private String fileName;
     private ArrayList<DisplayObject> frames;
-    private Integer currentFrame;
+    private Integer currentFrame = 0;
     private Integer startFrame;
     private Integer endFrame;
-    private Integer animationSpeed;
+    private Integer animationSpeed = DEFAULT_ANIMATION_SPEED;
     private GameClock gameClock;
 
 
 
     public AnimatedSprite(String id, String fileName, Point pos) {
-        super(id);
+        super(id, fileName);
         this.fileName = fileName;
         this.position = pos;
         this.gameClock = new GameClock();
+        this.loadFrames();
     }
 
 
@@ -43,26 +45,48 @@ public class AnimatedSprite extends Sprite {
         this.animationSpeed = animationSpeed;
     }
 
-    public void draw() {
-        currentFrame = startFrame;
-        while (currentFrame != endFrame) {
+    public Integer getAnimationSpeed() {
+        return animationSpeed;
+    }
 
-        }
+    @Override
+    public void draw(Graphics g) {
+            if (this.gameClock.getElapsedTime() >= this.animationSpeed) {
+                if (this.playing) {
+                    if (this.currentFrame == this.endFrame) {
+                        this.currentFrame = this.startFrame;
+                    } else {
+                        this.currentFrame++;
+                    }
+                }
+                this.setImage(this.frames.get(this.currentFrame).getDisplayImage());
+                this.gameClock.resetGameClock();
+            }
 
-
+        super.draw(g);
     }
 
     /* Krishan */
-    public void loadFrames() { /* AKA initializeFrames */
-        ArrayList paths = new ArrayList;
-        paths.add("mario_jump_0.png");
+    public void loadFrames()
+    { /* AKA initializeFrames */
+        this.frames = new ArrayList<DisplayObject>();
+        this.animations = new ArrayList<Animation>();
+        ArrayList paths = new ArrayList();
+        paths.add("mario_jump_0.png"); // mario at rest
         paths.add("mario_jump_1.png");
-        paths.add("mario_run_0.png");
+        paths.add("mario_jump_0.png");
         paths.add("mario_run_1.png");
-        for(int i = 0; i<4; i++) {
-            DisplayObject frame = new DisplayObject(paths[i], paths[i]);
+        paths.add("mario_run_0.png");
+        for(int i = 0; i<5; i++) {
+            DisplayObject frame = new DisplayObject(paths.get(i).toString(), paths.get(i).toString());
             this.frames.add(frame);
         }
+
+        Animation jump = new Animation("jump",1,2);
+        Animation run = new Animation("run",3,4);
+
+        this.animations.add(jump);
+        this.animations.add(run);
     }
 
     /* Krishan */
@@ -72,28 +96,50 @@ public class AnimatedSprite extends Sprite {
                 return this.animations.get(i);
             }
         }
+        System.out.println("Could not find animation with ID:");
+        throw new NoSuchElementException();
     }
 
 
     /* Partner 2 - Part 3 and 4 */
     public void animate(Animation animateObject) {
+        if(this.playing) {
+            return;
+        }
         this.startFrame = animateObject.getStartFrame();
         this.endFrame = animateObject.getEndFrame();
+        if(this.currentFrame == null || this.currentFrame > endFrame || this.currentFrame < startFrame) {
+            this.setImage(this.frames.get(this.startFrame).getDisplayImage());
+            this.currentFrame = this.startFrame;
+            this.gameClock.resetGameClock();
+        }
+        this.playing = true;
     }
     public void animate(String id) {
-
-
+        animate(getAnimation(id));
     }
     public void animate(Integer startFrame, Integer endFrame) {
+        if(this.playing) {
+            return;
+        }
         this.startFrame = startFrame;
         this.endFrame = endFrame;
+        if(this.currentFrame > endFrame || this.currentFrame < startFrame) {
+            this.setImage(this.frames.get(this.startFrame).getDisplayImage());
+            this.currentFrame = this.startFrame;
+            this.gameClock.resetGameClock();
+        }
+        this.playing = true;
     }
 
     public void stopAnimation(Integer frameNumber) {
-
+        this.setImage(this.frames.get(frameNumber).getDisplayImage());
+        this.currentFrame = frameNumber;
+        this.playing = false;
+        this.gameClock.resetGameClock();
     }
     public void stopAnimation() {
-
+        stopAnimation(this.startFrame);
     }
 
 
