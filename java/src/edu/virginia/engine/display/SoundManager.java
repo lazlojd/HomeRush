6adee8c7ced2e2955cf4a791dcd2af8911138710
Leslie Jones-Dove <ljonesdove@cs.uchicago.edu>
2018-11-04 13:45:22 -0600

    some sound implementation

--- java/src/edu/virginia/engine/display/SoundManager.java
+++ java/src/edu/virginia/engine/display/SoundManager.java
@@ -1,16 +1,50 @@
 package edu.virginia.engine.display;
 
-public class SoundManager {
+import javax.sound.sampled.*;
+import java.util.HashMap;
+import java.io.*;
 
+public class SoundManager {
+    private HashMap<String, File> soundEffects;
+    private Clip clip;
+
+    private AudioFileFormat generateAudioFile(String fileName) {
+        return New AudioFileFormat(New AudioFileFormat.Type(, "wav"))
+    }
     public SoundManager() {
+       this.soundEffects = new Hashmap<String, String>();
+       try {
+           this.clip = AudioSystem.getClip();
+       } catch (LineUnavailableException e) {
+           System.err.println(e);
+       }
 
     }
 
     public void LoadSoundEffect(String id, String filename) {
+        if (!this.soundEffects.containsKey(id)) {
+            String file = ("resources" + File.separator + fileName);
+            File fileObj = new File(file);
+            this.soundEffects.put(id, fileObj);
+                //AudioInputStream gameSound = AudioSystem.getAudioInputStream(fileObj);
+
+
+//            AudioFileFormat audioFile = AudioSystem.getAudioFileFormat(new File(file));
+
+        }
+
 
     }
 
     public void PlaySoundEffect(String id) {
+        File audioFile = this.soundEffects.get(id);
+        try {
+            AudioInputStream gameSound = AudioSystem.getAudioInputStream(fileObj);
+            clip.open(gameSound);
+            clip.start();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
 
     }
 
